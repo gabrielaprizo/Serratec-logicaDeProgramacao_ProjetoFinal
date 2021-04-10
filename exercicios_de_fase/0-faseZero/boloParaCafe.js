b00de54aa0265a8a2524ed6c01696b39d2d83d3a
Gabriela Prizo <gabbprizo@gmail.com>
2021-04-10 12:42:01 -0300

    Exercícios da fase zero

---
+++ exercicios_de_fase/0-faseZero/boloParaCafe.js
@@ -0,0 +1,25 @@
+/*O sistema “Bolo para o café” irá escrever na tela a receita de um bolo para o lanche da tarde, desde a separação dos ingredientes até o modo de preparo. O arquivo bolo.html deve conter uma seção ‘script’ e escrever a receita direto na página. Você pode utilizar marcações HTML para formatação.
+*/
+
+//Inicialização da Receita
+document.write('<b> ======  Receita de Bolo de fubá  ======<br></b>');
+document.write('O bolo de banana é feito com banana prata e possui um sabor maravilhoso.<br>');
+document.write('Ele pode ser servido frio ou quentinho.<br><br>') 
+
+//Ingredientes em lista
+document.write('<h1> Ingredientes:<br><br></h1>');
+document.write('1: 3 ovos<br>');
+document.write('2: 2 xícaras (chá) de açúcar<br>');
+document.write('3: 2 xícaras (chá) de fubá<br>');
+document.write('4: 3 colheres (sopa) rasas de farinha de trigo<br>');
+document.write('5: 1/2 copo (americano) de óleo<br>');
+document.write('6: 1 copo de leite<br>');
+document.write('7: 1 colher (sopa) de fermento em pó<br>');
+
+//Modo de preparo em lista
+document.write('<h2> Modo de Preparo </h2><br><br>')
+document.write('1: Bata todos os ingredientes no liquidificador.<br>');
+document.write('2: Coloque em uma forma untada e enfarinhada.<br>');
+document.write('3: Leve ao forno preaquecido e deixe assar, por cerca de 40 minutos.<br>');
+document.write(' === Sirva como preferir ===<br>');
+

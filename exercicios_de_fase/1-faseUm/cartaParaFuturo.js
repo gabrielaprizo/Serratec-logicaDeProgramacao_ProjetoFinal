f4a7483033d1c63538917f1eba4090ea56df1079
Gabriela Prizo <gabbprizo@gmail.com>
2021-04-10 12:42:18 -0300

    Exercício da fase Um

---
+++ exercicios_de_fase/1-faseUm/cartaParaFuturo.js
@@ -0,0 +1,16 @@
+/*Escreva uma carta para você mesmo ler ao final do curso, e imprima no console. Na carta você deve incluir as suas expectativas para o curso de programação e sua carreira nesta nova área. Seja criativo, lembre-se de incluir desafios e incentivos, para que o seu eu do futuro tenha orgulho de suas conquistas!
+*/
+//Um momento fofura :)
+
+console.log('== Carta para o Futuro == \n');
+console.log('Querida Gabriela do futuro, \n')
+console.log('Espero que você tenha se encontrado mais a cada dia no caminho que escolheu pra si.\n')
+console.log('E que tenha aprendido a administrar seu tempo, e a concluir mais rápido os seus exercícios de algoritmo.\n');
+console.log('E nunca tenha se esquecido que você é a autora da sua carreira, e tenha dado o seu melhor.\n');
+console.log('   Me conta, conseguiu o certificado Scrum.org? lol\n');
+console.log('Quando ler essa carta, lembre-se desse época, onde tudo ainda era amedrontador apesar de você não deixar transparecer\n')
+console.log('E depois de analisar tudo isso, se sinta vitoriosa por ter chegado aonde chegou.\n');
+console.log('E o que você não conseguiu/ alcançou, e te deixou com cicatrizes,');
+console.log('nunca sofra por isso, e lembre-se que as cicatrizes mostram as lutas em que você sobreviveu');
+console.log('Por ultimo, programadora (hahaha), nunca se esqueça de sorrir! \n');
+console.log('Um abraço quentinho da Gabriela do Passado.')
